ee676d93bb45a0317e86af5dd6cf1f4927fe1325
Glen Churchward <glenchurchward@gmail.com>
2019-02-21 19:54:09 +0000

    Update computing_gc_content.py

--- computing_gc_content.py
+++ computing_gc_content.py
@@ -4,27 +4,20 @@
 #TODO calculate GC content of DNA string
 #TODO equate value to dictionary
 #TODO move through dictionary looking for highest and return highest string and GC content
+from itertools import islice
 
 
 def file_open(file_path):
-    var = []
+    vari = []
     fasta = []
     with open(file_path, 'r') as f:
         g = f.read()
         for line in g:
-            var.append(line)
-    print(var)
-    #chevron = var.index('>')
-    chevron = [i for i, x in enumerate(var) if x == ">" or x == "\n"]
+            line = line.strip()
+            vari.append(line)
+
+    chevron = [i for i, x in enumerate(vari) if x == ">" or x == "\n"]
     print(chevron)
-
-    print(var)
-    x = []
-    y = chevron[0:1]
-    x.append((var[y]))
-    print(x)
-
-
-
+    print(vari)
 
 file_open('/Users/Glen/PycharmProjects/Rosalind/test_case.txt')
