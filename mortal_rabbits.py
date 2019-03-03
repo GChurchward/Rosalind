3f1b84c64478eb8963a9f5ac6e39bd0c1da4cbc9
Glen Churchward <glenchurchward@gmail.com>
2019-03-03 18:54:39 +0000

    corrected mortal rabbits to work

--- mortal_rabbits.py
+++ mortal_rabbits.py
@@ -16,16 +16,17 @@
 
 class rabbits(object):
     def __init__(self, n, k, m):
-        self.generation = 1
+        self.generation = 0
         self.child = 1
         self.mature = 0
         self.adult = 0
         self.litter = k
-        self.final_generation = n + 1
+        self.final_generation = n
         self.total = 0
         self.lifespan = m
         self.dead = 0
-        self.offspring = []
+        self.offspring = [1]
+        self.dead_list = []
 
     def child_aging(self):
         self.mature = self.child
@@ -40,19 +41,26 @@ class rabbits(object):
     def become_adults(self):
         self.adult += self.mature
         print(f'adults {self.adult}')
-        if (self.generation - 1) < (self.lifespan):
+        if (self.generation) < self.lifespan:
+            print('too soon for deaths')
             pass
         else:
-            self.dead = self.offspring[(self.generation - self.lifespan)]
+            self.dead = self.offspring[((self.generation) - self.lifespan)]
+            self.dead_list.append(self.dead)
             print(self.dead)
+            print(f' dead list is {self.dead_list}')
             self.adult = self.adult - self.dead
-        print(f'total dead {self.dead}')
-        print(f'total minus dead {self.adult}')
+        # print(f'total dead {self.dead}')
+        return
+        # print(f'total minus dead {self.adult}')
         return self.adult
+
+    # def dead(self):
+
 
     def next_generation(self):
         self.total = self.adult + self.child
-        #print(self.total)
+        # print(self.total)
         self.generation = self.generation + 1
         return
 
@@ -60,16 +68,18 @@ class rabbits(object):
         self.child_aging()
         self.offspring_born()
         self.become_adults()
+        # self.dead()
         self.next_generation()
         return
 
     def working_gen(self):
-        while self.generation in range(1, self.final_generation):
-            if self.generation == 1:
+        while self.generation in range(0, self.final_generation):
+            if self.generation == 0:
                 self.next_generation()
             else:
                 self.aging()
-            print('generation: ' + str(f'{self.generation}') + ' total: ' + str(f'{self.total}'))
+            print('generation: ' + str(f'{self.generation}') + ' total: ' + str(f'{self.total}')
+                  + ' mature: ' + str(f'{self.mature}') + ' childs: ' + str(f'{self.child}'))
         return
 
-rabbits(6, 1, 3).working_gen()
+rabbits(96, 1, 20).working_gen()
