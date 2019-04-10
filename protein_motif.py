fc369805236dd40ee725e792d9c956e367294861
Glen Churchward <glenchurchward@gmail.com>
2019-04-10 20:55:39 +0100

    a

--- protein_motif.py
+++ protein_motif.py
@@ -4,7 +4,33 @@ a script to take a list of proteins from a txt file, access UniProt protein data
 protein motif
     rosalind MPRT
 """
-from pip._vendor import urllib3
+import requests
+
+
+
+def protein_strip(request_string):
+
+    protein = []
+
+    for i in request_string:
+        protein.append(i)
+        print(i)
+    print(protein)
+    protein_concat = []
+    for i in protein:
+        if i == '\n':
+            protein_concat.append(protein[i + 1:])
+            print(protein_concat)
+         # line = i.strip()
+        #print(line)
+        # protein = []
+        # if request_string[j: j + 1] == 'SV':
+          #  print(line + 3)
+            # for j in request_string:
+            #     protein.append()
+            # # protein = line[index:]
+            # print(line)
+            # # print(protein)
 
 
 def file_open_to_list(file_path):
@@ -21,16 +47,17 @@ def file_open_to_list(file_path):
             proteins_id_list.append(line)
 
     print(proteins_id_list)
-    http = urllib3.PoolManager()
     for item in proteins_id_list:
         url = 'http://www.uniprot.org/uniprot/' + item + '.fasta'
-        req = http.request("GET", url)
+        req = requests.get(url)
         # print(req)
-        string = req.data.decode('utf-8')
+        string = req.content
         # print(string)
         protein_dict[item] = string
 
         print(protein_dict)
 
 
-file_open_to_list('/Users/Glen/Documents/protein_id_list.txt')
+# file_open_to_list('/Users/Glen/Documents/protein_id_list.txt')
+
+protein_strip("b'>sp|A2Z669|CSPLT_ORYSI CASP-like protein 5A2 OS=Oryza sativa subsp. indica OX=39946 GN=OsI_33147 PE=3 SV=1\nMRASRPVVHPVEAPPPAALAVAAAAVAVEAGVGAGGGAAAHGGENAQPRGVRMKDPPGAP\nGTPGGLGLRLVQAFFAAAALAVMASTDDFPSVSAFCYLVAAAILQCLWSLSLAVVDIYAL\nLVKRSLRNPQAVCIFTIGDGITGTLTLGAACASAGITVLIGNDLNICANNHCASFETATA\nMAFISWFALAPSCVLNFWSMASR\n'")
